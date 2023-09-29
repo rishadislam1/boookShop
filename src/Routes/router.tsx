4db2ea7dd6b4459d725344d765a1ce2f53bcbd4f
Rishad Islam <rishad.islam158@gmail.com>
2023-09-29 12:07:14 +0600

    all setup

---
+++ src/Routes/router.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Main from '../Layout/Main';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
+
+export default router;
